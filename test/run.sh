7de21618678e4bdcff2acdbb9e54c1454f959f95
Phil Davidov <phillip.davidov@hatch.co>
2025-01-30 15:22:19 -0600

    add logstash-filter-verifier test files

---
+++ test/run.sh
@@ -0,0 +1,2 @@
+#./logstash-filter-verifier standalone ../elk-docker/test/beats-nginx-test.json ../elk-docker/logstash-conf/02-beats-input.conf ../elk-docker/logstash-conf/30-output.conf
+./logstash-filter-verifier standalone ../elk-docker/test/beats-nginx-test.json /home/davidovp/workspace/git/elk-docker/config /home/davidovp/workspace/git/elk-docker/config/conf.d
